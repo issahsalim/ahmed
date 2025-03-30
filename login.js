dba23b33fde3cca74d89a3f0ae9217022d3d4c0d
Vote system <issahsalim233@gmail.com>
2025-03-30 20:11:54 +0000

    fix logins

--- login.js
+++ login.js
@@ -3,15 +3,16 @@ document.getElementById("adminLoginForm").addEventListener("submit", function (e
     let adminID = document.getElementById("adminID").value;
 
     let correctAdminID = [
-        "UEB3211422", "UEB3236322", "UEB3236422", "UEB3236522",
-        "UEB3236622", "UEB3236722", "UEB3237022", "UEB3237122 ",
-        "UEB3237222", "UEB3237322", "UEB3237422"
+        "UEB3237122", "UEB3211422", "UEB3236322", "UEB3236422", "UEB3236522",
+        "UEB3236622", "UEB3236722", "UEB3237022",
+        "UEB3237222", "UEB3237322", "UEB3237422", 
     ]
+
 
     if (correctAdminID.includes(adminID)) {
         window.location.href = "admin.html";
     } else {
-        Swal.fire({
+        Swal.fire({ 
             icon: "error",
             title: "You dey smoke ebo abii",
             text: document.getElementById("errorMessage").innerText = "Invalid Admin ID!",
@@ -19,3 +20,5 @@ document.getElementById("adminLoginForm").addEventListener("submit", function (e
         });
     }
 });  
+
+
